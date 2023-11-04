cb376eb229e0e27cc3ca769d86dbd93a7c41d1c4
Ivan Morales <124518614+ivxnmorxles@users.noreply.github.com>
2023-11-03 21:33:53 -0400

    Add files via upload

---
+++ Task.swift
@@ -0,0 +1,93 @@
+//
+//  Task.swift
+//
+
+import UIKit
+
+// The Task model
+struct Task: Codable {
+
+    // The task's title
+    var title: String
+
+    // An optional note
+    var note: String?
+
+    // The due date by which the task should be completed
+    var dueDate: Date
+    var id: String
+    // Initialize a new task
+    // `note` and `dueDate` properties have default values provided if none are passed into the init by the caller.
+    init(title: String, note: String? = nil, dueDate: Date = Date()) {
+        self.title = title
+        self.note = note
+        self.dueDate = dueDate
+        self.id = UUID().uuidString
+    }
+
+    // A boolean to determine if the task has been completed. Defaults to `false`
+    var isComplete: Bool = false {
+
+        // Any time a task is completed, update the completedDate accordingly.
+        didSet {
+            if isComplete {
+                // The task has just been marked complete, set the completed date to "right now".
+                completedDate = Date()
+            } else {
+                completedDate = nil
+            }
+        }
+    }
+
+    // The date the task was completed
+    // private(set) means this property can only be set from within this struct, but read from anywhere (i.e. public)
+    private(set) var completedDate: Date?
+
+    // The date the task was created
+    // This property is set as the current date whenever the task is initially created.
+    let createdDate: Date = Date()
+
+    // An id (Universal Unique Identifier) used to identify a task.
+    //let id: String = UUID().uuidString
+}
+
+// MARK: - Task + UserDefaults
+extension Task {
+
+
+    // Given an array of tasks, encodes them to data and saves to UserDefaults.
+    static func save(_ tasks: [Task]) {
+        // TODO: Save the array of tasks
+        let encoder = JSONEncoder();
+        let encodedTasks = try! encoder.encode(tasks)
+        UserDefaults.standard.set(encodedTasks,forKey:"tasksKey")
+    }
+
+    // Retrieve an array of saved tasks from UserDefaults.
+    static func getTasks() -> [Task] {
+        // TODO: Get the array of saved tasks from UserDefaults
+        if let tasksData = UserDefaults.standard.data(forKey: "tasksKey") {
+        let decoder = JSONDecoder()
+        let decodedTasks = try! decoder.decode([Task].self, from: tasksData)
+        return decodedTasks
+        }
+        return [] // 👈 replace with returned saved tasks
+    }
+
+    // Add a new task or update an existing task with the current task.
+    func save() {
+        // TODO: Save the current task
+        var existingTasks = Task.getTasks()
+        if let i = existingTasks.firstIndex(where:{ $0.id == self.id }){
+            //existingTasks[i] = self
+            existingTasks.remove(at: i)
+            existingTasks.insert(self, at: i)
+        }else{
+           //existingTasks[i] = self
+           //existingTasks[i].completedDate = self.completedDate
+            existingTasks.append(self)
+        }
+        Task.save(existingTasks)
+    }
+}
+
